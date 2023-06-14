076ada5b77ef64845b9c507319c8186309d6e6ba
Eren Yavuz <109018781+erenyavuzs@users.noreply.github.com>
2023-06-14 22:07:15 +0300

    second commit

--- app.js
+++ app.js
@@ -6,7 +6,58 @@ const toggleModal = () => {
 };
 
 const getShoes = () => {
-    fetch("./products.json")
-      .then((res) => res.json())
-      .then((shoes) => (shoeList = shoes));
+  fetch("./products.json")
+    .then((res) => res.json())
+    .then((shoes) => (shoeList = shoes));
 };
+
+getShoes(); 
+
+const createShoeStars = (starRate) => {
+    let starRateHtml = "";
+    for(let i =1; i<=5;i++){
+        if(Math.round(starRate) >= i) starRateHtml += `<i class="bi bi-star-fill"></i>`;
+    }
+
+    return starRateHtml;
+};
+
+const createShoeItemsHtml = () => {
+    const shoeListEl = document.querySelector(".shoe_list");
+    let shoeListHtml = "";
+    shoeList.forEach(shoe, index => {
+        shoeListHtml += `<div class="col-5 ${index % 2 == 0 && "offeset-2"}">
+        <div class="row shoe_card">
+          <div class="col-6">
+            <img class="img-fluid shadow" src="${shoe.imgSource}" width="250" height="250"/>
+            </div>
+            <div class="col-6">
+              <div class="shoe_detail">
+                <div>
+                  <span class="shoe_name">${shoe.name}Nike Downshifter 12</span>
+                  <br>
+                  <span class="shoe_reivew-text">23 reivews</span>
+                  <span class="shoe_star-rate">
+                    ${createShoeStars()}
+                  </span>
+                </div>
+                <p class="shoe_description">
+                  ${shoe.description}
+                </p>
+                <div>
+                  <span class="new_price">${shoe.price}$</span>
+                  ${shoe.oldPrice && `<span class="old_price">${shoe.oldPrice}</span>`}
+                  <button class="add_button">ADD BASKET</button>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>`;
+    });
+
+    shoeListEl.innerHtml = shoeListHtml;
+};
+
+setTimeout(() => {
+    createShoeItemsHtml();  
+}, 100); 
