452ca054c4773e55c96a92f0f96368700a14731f
Eren Yavuz <109018781+erenyavuzs@users.noreply.github.com>
2023-10-13 00:06:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ const toggleModal = () => {
 };
 
 const getShoes = () => {
-  fetch("./products.json")
+  fetch("products.json")
     .then((res) => res.json())
     .then((shoes) => (shoeList = shoes));
 };
 
-getShoes(); 
+getShoes();
 
 const createShoeStars = (starRate) => {
-    let starRateHtml = "";
-    for(let i =1; i<=5;i++){
-        if(Math.round(starRate) >= i) starRateHtml += `<i class="bi bi-star-fill"></i>`;
-    }
+  let starRateHtml = "";
+  for(let i =1; i<=5;i++){
+      if(Math.round(starRate) >= i) starRateHtml += `<i class="bi bi-star-fill"></i>`;
+  }
 
-    return starRateHtml;
+  return starRateHtml;
 };
 
 const createShoeItemsHtml = () => {
     const shoeListEl = document.querySelector(".shoe_list");
     let shoeListHtml = "";
-    shoeList.forEach(shoe, index => {
-        shoeListHtml += `<div class="col-5 ${index % 2 == 0 && "offeset-2"}">
+    shoeList.forEach((shoe, index) => {
+        shoeListHtml += `<div class="col-5 ${index % 2 == 0 && "offset-2"}">
         <div class="row shoe_card">
           <div class="col-6">
             <img class="img-fluid shadow" src="${shoe.imgSource}" width="250" height="250"/>
@@ -34,18 +34,18 @@ const createShoeItemsHtml = () => {
             <div class="col-6">
               <div class="shoe_detail">
                 <div>
-                  <span class="shoe_name">${shoe.name}Nike Downshifter 12</span>
+                  <span class="shoe_name">${shoe.name}</span>
                   <br>
                   <span class="shoe_reivew-text">23 reivews</span>
                   <span class="shoe_star-rate">
-                    ${createShoeStars()}
+                  ${createShoeStars()}
                   </span>
                 </div>
                 <p class="shoe_description">
                   ${shoe.description}
                 </p>
                 <div>
-                  <span class="new_price">${shoe.price}$</span>
+                  <span class="new_price">${shoe.price}</span>
                   ${shoe.oldPrice && `<span class="old_price">${shoe.oldPrice}</span>`}
                   <button class="add_button">ADD BASKET</button>
                 </div>
@@ -55,9 +55,9 @@ const createShoeItemsHtml = () => {
       </div>`;
     });
 
-    shoeListEl.innerHtml = shoeListHtml;
+  shoeListEl.innerHTML = shoeListHtml;
 };
 
 setTimeout(() => {
-    createShoeItemsHtml();  
-}, 100); 
+  createShoeItemsHtml();
+}, 100);
