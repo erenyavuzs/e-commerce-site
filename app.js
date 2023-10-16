2e0640ac82e917b19dd7ba35a1ffb5fcea75b61a
Eren Yavuz <109018781+erenyavuzs@users.noreply.github.com>
2023-10-16 21:48:17 +0300

    Update

--- app.js
+++ app.js
@@ -6,58 +6,107 @@ const toggleModal = () => {
 };
 
 const getShoes = () => {
-  fetch("products.json")
-    .then((res) => res.json())
-    .then((shoes) => (shoeList = shoes));
+    fetch("products.json")
+        .then((res) => res.json())
+        .then((shoes) => (shoeList = shoes))
+        .then(() => {
+            createShoeItemsHtml();
+            createShoeTypesHtml();
+        });
 };
 
 getShoes();
 
 const createShoeStars = (starRate) => {
-  let starRateHtml = "";
-  for(let i =1; i<=5;i++){
-      if(Math.round(starRate) >= i) starRateHtml += `<i class="bi bi-star-fill"></i>`;
-  }
+    let starRateHtml = "";
+    for (let i = 1; i <= 5; i++) {
+        if (Math.round(starRate) >= i) starRateHtml += `<i class="bi bi-star-fill"></i>`;
+    }
 
-  return starRateHtml;
+    return starRateHtml;
 };
 
-const createShoeItemsHtml = () => {
+const createShoeItemsHtml = (shoes = shoeList) => {
     const shoeListEl = document.querySelector(".shoe_list");
     let shoeListHtml = "";
-    shoeList.forEach((shoe, index) => {
-        shoeListHtml += `<div class="col-5 ${index % 2 == 0 && "offset-2"}">
-        <div class="row shoe_card">
-          <div class="col-6">
-            <img class="img-fluid shadow" src="${shoe.imgSource}" width="250" height="250"/>
-            </div>
-            <div class="col-6">
-              <div class="shoe_detail">
-                <div>
-                  <span class="shoe_name">${shoe.name}</span>
-                  <br>
-                  <span class="shoe_reivew-text">23 reivews</span>
-                  <span class="shoe_star-rate">
-                  ${createShoeStars()}
-                  </span>
+    shoes.forEach((shoe, index) => {
+        shoeListHtml += `<div class="col-5 ${index % 2 === 0 ? "offset-2" : ""}">
+            <div class="row shoe_card">
+                <div class="col-6">
+                    <img class="img-fluid shadow" src="${shoe.imgSource}" width="250" height="250" />
                 </div>
-                <p class="shoe_description">
-                  ${shoe.description}
-                </p>
-                <div>
-                  <span class="new_price">${shoe.price}</span>
-                  ${shoe.oldPrice && `<span class="old_price">${shoe.oldPrice}</span>`}
-                  <button class="add_button">ADD BASKET</button>
+                <div class="col-6">
+                    <div class="shoe_detail">
+                        <div>
+                            <span class="shoe_name">${shoe.name}</span>
+                            <br>
+                            <span class="shoe_review-text">23 reviews</span>
+                            <span class="shoe_star-rate">
+                                ${createShoeStars(shoe.starRate)}
+                            </span>
+                        </div>
+                        <p class="shoe_description">
+                            ${shoe.description}
+                        </p>
+                        <div>
+                            <span class="new_price">${shoe.price}</span>
+                            ${
+            shoe.oldPrice ?
+                `<span class="old_price">${shoe.oldPrice}</span>` : ""
+            }
+                            <button class="add_button">ADD BASKET</button>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-        </div>
-      </div>`;
+        </div>`;
     });
 
-  shoeListEl.innerHTML = shoeListHtml;
+    shoeListEl.innerHTML = shoeListHtml;
+};
+
+const Shoe_Types = {
+    ALL: "All",
+    SPORT: "Sport",
+    BOOT: "Boot",
+    SNEAKER: "Sneaker",
+    CLASSIC: "Classic"
+};
+
+const createShoeTypesHtml = () => {
+    const filterEl = document.querySelector(".filter");
+    let filterHtml = "";
+    let filterTypes = ["ALL"];
+    shoeList.forEach(shoe => {
+        if (filterTypes.indexOf(shoe.type) === -1)
+            filterTypes.push(shoe.type);
+    });
+
+    filterTypes.forEach((type, index) => {
+        filterHtml += `<li class="${
+            index === 0 ? "active" : ""
+        }" onclick="filterShoes(this)" data-type="${type}">${Shoe_Types[type] || type}</li>`;
+    });
+
+    filterEl.innerHTML = filterHtml;
+};
+
+const filterShoes = (filterEl) => {
+    document.querySelector(".filter .active").classList.remove("active");
+    filterEl.classList.add("active");
+    let shoeType = filterEl.dataset.type;
+    let filteredShoes;
+
+    if (shoeType !== "ALL") {
+        filteredShoes = shoeList.filter((shoe) => shoe.type === shoeType);
+    } else {
+        filteredShoes = [...shoeList];
+    }
+
+    createShoeItemsHtml(filteredShoes);
 };
 
 setTimeout(() => {
-  createShoeItemsHtml();
+    createShoeItemsHtml();
+    createShoeTypesHtml();
 }, 50);
